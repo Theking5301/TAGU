4f7b160aa080240c9edff074f00b850d80cae843
Theking530 <amine@suburbandigital.net>
2015-10-20 17:04:35 -0400

    Vectors

--- app/src/main/java/com/suburbandigital/amine/tagu/Tags/Tag.java
+++ app/src/main/java/com/suburbandigital/amine/tagu/Tags/Tag.java
@@ -1,5 +1,7 @@
 package com.suburbandigital.amine.tagu.Tags;
 
+import com.suburbandigital.amine.tagu.Math.Vec2;
+
 /**
  * Created by Amine on 10/20/2015.
  */
@@ -7,15 +9,24 @@ public class Tag {
     private String NAME;
     private String DESCRIPTION;
     private TagType TYPE;
-    private double X;
-    private double Y;
+    private Vec2 POSITION;
 
+
+    public Tag(String name, String description, TagType type, Vec2 position) {
+        NAME = name;
+        DESCRIPTION = description;
+        TYPE = type;
+        POSITION = position;
+    }
+
+    /**
+     * Creates a Tag using X and Y coordinates instead of a vector.
+     */
     public Tag(String name, String description, TagType type, double x, double y) {
         NAME = name;
         DESCRIPTION = description;
         TYPE = type;
-        X = x;
-        Y = y;
+        POSITION = new Vec2(x,y);
     }
     public String getNAME() {
         return NAME;
@@ -26,10 +37,13 @@ public class Tag {
     public TagType getTYPE() {
         return TYPE;
     }
+    public Vec2 getPOSITION() {
+        return POSITION;
+    }
     public double getX() {
-        return X;
+        return POSITION.getX();
     }
     public double getY() {
-        return Y;
+        return POSITION.getY();
     }
 }
---
+++ app/src/main/java/com/suburbandigital/amine/tagu/Tags/UserPosition.java
@@ -0,0 +1,26 @@
+package com.suburbandigital.amine.tagu.Tags;
+
+import com.suburbandigital.amine.tagu.Math.Vec2;
+
+/**
+ * Created by Amine on 10/20/2015.
+ */
+public class UserPosition {
+    private Vec2 POSITION;
+
+    public UserPosition(double x, double y) {
+        POSITION = new Vec2(x,y);
+    }
+    public UserPosition(Vec2 position) {
+        POSITION = position;
+    }
+    public double getX() {
+        return POSITION.getX();
+    }
+    public double getY() {
+        return POSITION.getY();
+    }
+    public Vec2 getPOSITION() {
+        return POSITION;
+    }
+}
