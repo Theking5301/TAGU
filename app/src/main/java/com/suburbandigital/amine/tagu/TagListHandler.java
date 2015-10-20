4ca6c44879e5db57f52b5dfda683d1a0d36fa187
Theking530 <amine@suburbandigital.net>
2015-10-20 18:20:39 -0400

    Database Basics

--- app/src/main/java/com/suburbandigital/amine/tagu/TagListHandler.java
+++ app/src/main/java/com/suburbandigital/amine/tagu/TagListHandler.java
@@ -10,13 +10,13 @@ import android.database.sqlite.SQLiteOpenHelper;
  * TagU Closed Source
  * Created by Amine on 10/20/2015.
  */
-public class TagList extends SQLiteOpenHelper {
+public class TagListHandler extends SQLiteOpenHelper {
     private static final String DATABASENAME = "TagDatabase";
     private static final int DATABASEVERSION = 1;
     public SQLiteDatabase WRITABLEDB;
     public SQLiteDatabase READABLEDB;
 
-    public TagList(Context context) {
+    public TagListHandler(Context context) {
         super(context, DATABASENAME, null, DATABASEVERSION);
     }
     @Override
