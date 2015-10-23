d1c3b7f4283596f3b04b5679bff95edfca011810
Theking530 <amine@suburbandigital.net>
2015-10-22 20:05:01 -0400

    Starting on Database Work

--- app/src/main/java/com/suburbandigital/amine/tagu/TagDatabaseHandler.java
+++ app/src/main/java/com/suburbandigital/amine/tagu/TagDatabaseHandler.java
@@ -20,15 +20,14 @@ import java.util.List;
 public class TagDatabaseHandler extends SQLiteOpenHelper {
     private static final String DATABASENAME = "TagDatabase";
     private static final int DATABASEVERSION = 1;
-    public SQLiteDatabase DATABASE;
 
     private static final String KEY_ID = "id";
     private static final String KEY_NAME = "name";
     private static final String KEY_DESC = "desc";
     private static final String KEY_TYPE = "type";
     private static final String KEY_ENT = "ent";
-    private static final String KEY_XPOS = "xpos";
-    private static final String KEY_YPOS = "ypos";
+    private static final String KEY_XPOS = "posx";
+    private static final String KEY_YPOS = "posy";
 
     public TagDatabaseHandler(Context context) {
         super(context, DATABASENAME, null, DATABASEVERSION);
@@ -45,6 +44,7 @@ public class TagDatabaseHandler extends SQLiteOpenHelper {
     }
     public List<Tag> getAllTags() {
         List<Tag> tags = new LinkedList<Tag>();
+        addTag(new Tag("Wang Center", "Asian food and culture", "SBU", TagType.BUILDING, 40.9159, -73.1197));
         String query = "SELECT  * FROM " + DATABASENAME;
 
         SQLiteDatabase db = this.getWritableDatabase();
@@ -60,22 +60,23 @@ public class TagDatabaseHandler extends SQLiteOpenHelper {
                 tags.add(tag);
             } while (cursor.moveToNext());
         }
-        tags.add(new Tag("Wang Center", "Asian food and culture", "SBU", TagType.BUILDING, 40.9159, -73.1197));
         //Log.d("getAllBooks()", tag.toString());
         return tags;
     }
     public void addTag(Tag tag){
         Log.d("addTag", tag.toString());
+        SQLiteDatabase database = this.getWritableDatabase();
 
         ContentValues values = new ContentValues();
-        values.put(KEY_ID, tag.getID()); // get title
-        values.put(KEY_NAME, tag.getNAME()); // get author
-        values.put(KEY_DESC, tag.getDESCRIPTION()); // get title
-        values.put(KEY_TYPE, tag.getTYPE().toString()); // get author
-        values.put(KEY_ENT, tag.getENTITY()); // get title
-        values.put(KEY_XPOS, tag.getLat()); // get author
-        values.put(KEY_YPOS, tag.getLong()); // get title
+        values.put(KEY_ID, tag.getID());
+        values.put(KEY_NAME, tag.getNAME());
+        values.put(KEY_DESC, tag.getDESCRIPTION());
+        values.put(KEY_TYPE, tag.getTYPE().toString());
+        values.put(KEY_ENT, tag.getENTITY());
+        values.put(KEY_XPOS, tag.getLat());
+        values.put(KEY_YPOS, tag.getLong());
 
-        DATABASE.insert(DATABASENAME, null, values);
+        database.insert(DATABASENAME, null, values);
+        database.close();
     }
 }
