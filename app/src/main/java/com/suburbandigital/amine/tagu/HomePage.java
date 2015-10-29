81c409b585c85d367a3e79f178179405d267978f
Amine <amine@suburbandigital.net>
2015-10-29 03:58:21 -0400

    Discover

--- app/src/main/java/com/suburbandigital/amine/tagu/HomePage.java
+++ app/src/main/java/com/suburbandigital/amine/tagu/HomePage.java
@@ -20,7 +20,6 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.suburbandigital.amine.tagu.Map.MapManager;
@@ -30,17 +29,17 @@ import com.google.android.gms.maps.GoogleMap.OnMarkerClickListener;
 import com.suburbandigital.amine.tagu.Tags.TagType;
 
 
-public class MainMap extends FragmentActivity implements OnMapReadyCallback,
+public class HomePage extends FragmentActivity implements OnMapReadyCallback,
         GoogleApiClient.ConnectionCallbacks,
         GoogleApiClient.OnConnectionFailedListener,
         LocationListener {
 
-    private GoogleMap mMap;
-    private MapManager manager;
-    private GoogleApiClient mGoogleApiClient;
-    public static final String TAG = MainMap.class.getSimpleName();
+    private GoogleMap MAP;
+    private MapManager MANAGER;
+    private GoogleApiClient API_CLIENT;
+    public static final String TAG = HomePage.class.getSimpleName();
     private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
-    private LocationRequest mLocationRequest;
+    private LocationRequest LOCATION_REQUEST;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,12 +50,12 @@ public class MainMap extends FragmentActivity implements OnMapReadyCallback,
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-        mLocationRequest = LocationRequest.create()
+        LOCATION_REQUEST = LocationRequest.create()
                 .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
                 .setInterval(10 * 100)        // 1 seconds, in milliseconds
                 .setFastestInterval(1000); // 1 second, in milliseconds
 
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
+        API_CLIENT = new GoogleApiClient.Builder(this)
                 .addConnectionCallbacks(this)
                 .addOnConnectionFailedListener(this)
                 .addApi(LocationServices.API)
@@ -65,7 +64,7 @@ public class MainMap extends FragmentActivity implements OnMapReadyCallback,
         button.setOnClickListener(
                 new Button.OnClickListener() {
                     public void onClick(View v) {
-                        Location location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+                        Location location = LocationServices.FusedLocationApi.getLastLocation(API_CLIENT);
                         Tag tag;
                         TextView text = (TextView)findViewById(R.id.editText);
                         if(location != null) {
@@ -73,9 +72,9 @@ public class MainMap extends FragmentActivity implements OnMapReadyCallback,
                         }else{
                             tag = new Tag(text.getText().toString(), "Custom Tag", "SBU", TagType.BUILDING, 0, 0);
                         }
-                        manager.addTagToDB(tag);
-                        mMap.clear();
-                        manager.refreshMap();
+                        MANAGER.addTagToDB(tag);
+                        MAP.clear();
+                        MANAGER.refreshMap();
                     }
                 }
         );
@@ -83,7 +82,7 @@ public class MainMap extends FragmentActivity implements OnMapReadyCallback,
         button2.setOnClickListener(
                 new Button.OnClickListener() {
                     public void onClick(View v) {
-                        manager.removeMarker(manager.getSelectedMarker());
+                        MANAGER.removeMarker(MANAGER.getSelectedMarker());
                     }
                 }
         );
@@ -91,22 +90,22 @@ public class MainMap extends FragmentActivity implements OnMapReadyCallback,
         button3.setOnClickListener(
                 new Button.OnClickListener() {
                     public void onClick(View v) {
-                        manager.clearDB();
-                        mMap.clear();
+                        MANAGER.clearDB();
+                        MAP.clear();
                     }
                 }
         );
     }
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-        manager = new MapManager(getApplicationContext(), mMap);
+        MAP = googleMap;
+        MANAGER = new MapManager(getApplicationContext(), MAP);
         final View bottomFrame = findViewById(R.id.BottomFrame);
         bottomFrame.setVisibility(View.GONE);
         bottomFrame.setAlpha(0f);
         //bottomFrame.setTranslationY(300);
-        mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
-        mMap.setOnMarkerClickListener(
+        MAP.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        MAP.setOnMarkerClickListener(
                 new OnMarkerClickListener() {
                     @Override
                     public boolean onMarkerClick(Marker marker) {
@@ -118,11 +117,11 @@ public class MainMap extends FragmentActivity implements OnMapReadyCallback,
                         desc.setText(marker.getSnippet());
                         TextView pos = (TextView) findViewById(R.id.Pos);
                         pos.setText(marker.getPosition().toString());
-                        manager.setSelectedMarker(marker);
+                        MANAGER.setSelectedMarker(marker);
                         return false;
                     }
                 });
-        mMap.setOnMapClickListener(
+        MAP.setOnMapClickListener(
                 new GoogleMap.OnMapClickListener() {
                     @Override
                     public void onMapClick(LatLng latLng) {
@@ -136,35 +135,35 @@ public class MainMap extends FragmentActivity implements OnMapReadyCallback,
                                         bottomFrame.setVisibility(View.GONE);
                                     }
                                 });
-                        manager.setSelectedMarker(null);
+                        MANAGER.setSelectedMarker(null);
                     }
                 }
         );
-        mMap.setInfoWindowAdapter(new TagMarkerInfo(getApplicationContext()));
-        manager.refreshMap();
-        mMap.setMyLocationEnabled(true);
-        mMap.getUiSettings().setMyLocationButtonEnabled(false);
-        mMap.getUiSettings().setMapToolbarEnabled(false);
+        MAP.setInfoWindowAdapter(new TagMarkerInfo(getApplicationContext()));
+        MANAGER.refreshMap();
+        MAP.setMyLocationEnabled(true);
+        MAP.getUiSettings().setMyLocationButtonEnabled(false);
+        MAP.getUiSettings().setMapToolbarEnabled(false);
     }
     @Override
     protected void onResume() {
         super.onResume();
-        mGoogleApiClient.connect();
+        API_CLIENT.connect();
     }
     @Override
     protected void onPause() {
         super.onPause();
-        if (mGoogleApiClient.isConnected()) {
-            LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
-            mGoogleApiClient.disconnect();
+        if (API_CLIENT.isConnected()) {
+            LocationServices.FusedLocationApi.removeLocationUpdates(API_CLIENT, this);
+            API_CLIENT.disconnect();
         }
     }
     @Override
     public void onConnected(Bundle bundle) {
-        Location location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        Location location = LocationServices.FusedLocationApi.getLastLocation(API_CLIENT);
         Log.i(TAG, "Connection has been established");
         if (location == null) {
-            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+            LocationServices.FusedLocationApi.requestLocationUpdates(API_CLIENT, LOCATION_REQUEST, this);
         } else {
             handleNewLocation(location);
         }
@@ -199,9 +198,9 @@ public class MainMap extends FragmentActivity implements OnMapReadyCallback,
 //        MarkerOptions options = new MarkerOptions()
 //                .position(latLng)
 //                .title("I am here!");
-//        mMap.addMarker(options);
+//        MAP.addMarker(options);
 
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
+        MAP.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
     }
     @Override
     public void onLocationChanged(Location location) {
